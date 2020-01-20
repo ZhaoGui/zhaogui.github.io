da7cbd17e0297445db3a57052e4b66a007e9dc4b
Gui Zhao <562953985@qq.com>
2020-01-20 18:01:17 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "北林有涧"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "猎暄"
+email = "alan.gui@foxmail.com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "北林有涧风有时"
+key_words = ['Maverick', '猎暄', '北林', 'blog']
 language = 'zh-CN'
 external_links = [
     {
